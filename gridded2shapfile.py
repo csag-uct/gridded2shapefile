945502798b5e7c6090976d8703bec2c9e9e62012
Chris Jack <cjack@csag.uct.ac.za>
2016-04-29 15:57:47 +0200

    Added comments and cleaned up some redundent stuff

--- gridded2shapfile.py
+++ gridded2shapfile.py
@@ -7,14 +7,18 @@ import argparse
 
 import gridfunctions
 
+
+# Read source/template shapefile
 shpfile = fiona.open(sys.argv[1])
 feature_count = len(shpfile)
 print("{} features found".format(feature_count))
 
+# Open source netcdf data file
 ncfile = xray.open_dataset(sys.argv[2])
 print ncfile.data_vars
 print ncfile.coords
 
+# Try and get the latitude/longitude variables
 try:
 	lats = ncfile['lat']
 	lons = ncfile['lon']
@@ -22,16 +26,18 @@ except:
 	lats = ncfile['latitude']
 	lons = ncfile['longitude']
 
+# Construct full 2D lat/lon grids
 longrid, latgrid = np.meshgrid(lons, lats)
 
-# Get grid and shapes
+# Construct polygon grid
 shape, grid_polys = gridfunctions.makegrid(lats, lons)
 print("grid has shape {}".format(repr(shape)))
 
-# We'll keep all the weights
+# We'll keep all the weights, we could write these out as well
 weights = np.zeros((len(shpfile), shape[0], shape[1]), dtype=np.float32)
 
-# Create the output shapefile based on the source shapefile
+# Create the output shapefile based on the source shapefile schema
+
 schema = shpfile.schema
 
 for varname in ncfile.data_vars.keys():
@@ -42,26 +48,21 @@ print schema
 outshpfile = fiona.open(sys.argv[3], 'w', driver=shpfile.driver, crs=shpfile.crs, schema=schema)
 
 
+# Okay here, we go, iterate through the template features
 feature_id = 0
 patches = []
 for feature in shpfile:
 
-	print type(feature)
-	print feature['properties']	
+	# Get the actual geometry
 	shape = shapely.geometry.shape(feature['geometry'])
 	
-	try:
-		patches.append(PolygonPatch(shape, fc='green', ec='#555555', lw=0.2, alpha=1., zorder=100))
-	except:
-		pass
-
-	# Sweep the grid (this could be more efficient by pre-culling)
+	# Sweep the grid (this could be much more efficient by pre-culling based on geometry bounding box)
 	x, y = 0, 0
-	for row in grid_polys:		
+	for row in grid_polys:
 		for poly in row:
 			if poly.intersects(shape):
 				weights[feature_id,y,x] = poly.intersection(shape).area / poly.area
-				weights[feature_id] = weights[feature_id]/np.sum(weights[feature_id])
+				weights[feature_id] = weights[feature_id]/np.sum(weights[feature_id])  # Normalize to sum = 1
 			x += 1
 		
 		x = 0
@@ -73,9 +74,7 @@ for feature in shpfile:
 			value = np.sum(ncfile[varname][ti] * weights[feature_id])
 			feature['properties']['{}_t{}'.format(varname, ti+1)] = float(value)
 		
-		#print "writing"
 		print feature['properties']
-		#print outshpfile.schema
 		outshpfile.write(feature)
 
 
